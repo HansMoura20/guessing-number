90ce40ea6284bf0a5686576361d8fceb18b906c9
HansMoura20 <hansadrienmoura@outlook.com>
2020-12-22 14:42:36 +0800

    input value to match the correct secret number

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } 
+  else if(guess === secretNumber){
+    document.querySelector('.message').textContent = 'Correct Number!';
+  }
+});
